179b3a0f75b92ff04caa296615a6b953e95862e0
patry <xkaniax@gmail.com>
2022-01-25 17:39:19 +0100

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import './Navbar.css';
+import React, { useState } from 'react';
+import { BsBell } from 'react-icons/bs';
+
+
+const Navbar  = () => {
+
+    let isOpen = false;
+
+    const toggleMenu = () => {
+        let menu = document.getElementById('hamburger-menu');
+        let hamburgerIcon = document.getElementById('hamburger-icon');
+
+        if( isOpen ) {
+            menu.classList.remove('hamburger-menu--open');
+            hamburgerIcon.classList.remove('hamburger-icon--open');
+        }
+        else {
+            menu.classList.add('hamburger-menu--open');
+            hamburgerIcon.classList.add('hamburger-icon--open');
+        }    
+
+        isOpen = !isOpen;
+    }
+
+    return (  
+        <nav className='navbar'>
+            <div className='navbar-content'>
+                <div className='logo'>T</div>
+                <div className='navbar-menu'>
+                    <div id='hamburger-icon' className='hamburger-icon' onClick={toggleMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div id='hamburger-menu' className='hamburger-menu'>
+                        <a>Home</a>
+                        <a>Terrariums</a>
+                        <a>Settings</a>
+                        <a>Logout</a>
+                    </div>
+                    <button id='notification-bell'>
+                        <BsBell />
+                    </button>
+                </div>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
