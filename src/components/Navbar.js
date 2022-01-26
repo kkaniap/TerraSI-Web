f08a7a0d2e937e224e1951a7117e80551ac373ed
patry <xkaniax@gmail.com>
2022-01-26 15:34:47 +0100

    navbar finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,44 @@
 import './Navbar.css';
-import React, { useState } from 'react';
 import { BsBell, BsFillExclamationCircleFill } from 'react-icons/bs';
-
+import Notifications from './Notifications';
+import { useState, useEffect } from 'react';
 
 const Navbar  = () => {
 
-    let isOpen = false;
-
-    const toggleMenu = () => {
+    const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+    const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
+    
+    useEffect(() => {
         let menu = document.getElementById('hamburger-menu');
         let hamburgerIcon = document.getElementById('hamburger-icon');
 
-        if( isOpen ) {
-            menu.classList.remove('hamburger-menu--open');
-            hamburgerIcon.classList.remove('hamburger-icon--open');
-        }
-        else {
+        if( isHamburgerOpen ) {
             menu.classList.add('hamburger-menu--open');
             hamburgerIcon.classList.add('hamburger-icon--open');
+        }
+        else {
+            menu.classList.remove('hamburger-menu--open');
+            hamburgerIcon.classList.remove('hamburger-icon--open');
         }    
+    }, [isHamburgerOpen]);
 
-        isOpen = !isOpen;
-    }
+    useEffect(() => {
+        let notifications = document.getElementById('notifications');
 
-    const toggleNotification = () => {
-        console.log('kania123');
-    }
+        if( isNotificationsOpen ) {
+            notifications.classList.add('notifications--show');
+        }
+        else {
+            notifications.classList.remove('notifications--show');
+        }
+    }, [isNotificationsOpen]);
 
     return (  
         <nav className='navbar'>
             <div className='navbar-content'>
                 <div className='logo'>T</div>
                 <div className='navbar-menu'>
-                    <div id='hamburger-icon' className='hamburger-icon' onClick={toggleMenu}>
+                    <div id='hamburger-icon' className='hamburger-icon' onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>
                         <span></span>
                         <span></span>
                         <span></span>
@@ -43,10 +49,10 @@ const Navbar  = () => {
                         <a>Settings</a>
                         <a>Logout</a>
                     </div>
-                    <button id='notification-bell' onClick={toggleNotification}>
+                    <button id='notification-bell' onClick={() => setIsNotificationsOpen(!isNotificationsOpen)}>
                         <BsBell />
                     </button>
-                    <div className='list-menu'>
+                    <div className='desktop-menu'>
                         <a>Home</a>
                         <a>Terrariums</a>
                         <a>Settings</a>
@@ -54,27 +60,7 @@ const Navbar  = () => {
                     </div>
                 </div>
             </div>
-            <div id='notifications'>
-                <a className='notification'>
-                    <div className='notification__icon'>
-                        <BsFillExclamationCircleFill />
-                    </div>
-                    <div className='notification__content'>
-                        <b>Name</b><br />
-                        <span>High temperature in terrarium</span>
-                    </div>
-                </a>
-
-                <a className='notification'>
-                    <div className='notification__icon'>
-                        <BsFillExclamationCircleFill />
-                    </div>
-                    <div className='notification__content'>
-                        <b>Name</b><br />
-                        <span>High temperature in terrarium</span>
-                    </div>
-                </a>
-            </div>
+            <Notifications changeIsOpen={open => setIsNotificationsOpen(open)}/>
         </nav>
     );
 }
