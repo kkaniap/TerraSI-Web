7c99282c140632bcb472355f8758fd544f59f3b7
patry <xkaniax@gmail.com>
2022-01-28 20:40:57 +0100

    newsItemContent from html

--- src/components/newsItem/NewsItem.js
+++ src/components/newsItem/NewsItem.js
@@ -1,7 +1,22 @@
 import './NewsItem.css';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import DOMPurify from 'dompurify';
 
 const News = () => {
+
+    const [newsItemContent, setNewsItemContent] = useState();
+
+    useEffect(() => {
+        fetchNewsItemContent();
+    }, []);
+
+    const fetchNewsItemContent = () => {
+        fetch('/newsText/newsItem_1.html')
+        .then(data => data.text())
+        .then(data => setNewsItemContent(data))
+    }
+
     return (
         <Link to="/news/1" className='hiperlink'>
             <div className='news-container'>
@@ -9,8 +24,7 @@ const News = () => {
                     <img src='http://terrasi.herokuapp.com/assets/homeAssets/NewsID/3/logo_3.png' />
                 </div>
                 <h2 className='news__title'>How to save money with TerraSI</h2>
-                <div className='news__content'>
-                    We are pleased to inform you that we have started cooperation with ReptileSupply. This will contribute to better quality of our terrariums.If you want to know more about our cooperation, please visit this post.
+                <div className='news__content' dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(newsItemContent)}}>
                 </div>
             </div>
         </Link>
