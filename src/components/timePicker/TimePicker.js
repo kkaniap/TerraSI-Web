cd280d65731b1557c54afd8d9871d7a133516fd3
patry <xkaniax@gmail.com>
2022-02-04 14:41:58 +0100

    input validation added

--- src/components/timePicker/TimePicker.js
+++ src/components/timePicker/TimePicker.js
@@ -7,6 +7,26 @@ const TimePicker = (props) => {
 
     const [timeType, setTimeType] = useState('AM');
 
+    const hoursValidation = (e) => {
+        if(e.target.value > 12 || e.target.value < 0){
+            e.target.classList.add('time-picker-error');
+            return false;
+        }
+
+        e.target.classList.remove('time-picker-error');
+        return true;
+    }
+
+    const minutesValidation = (e) => {
+        if(e.target.value > 59 || e.target.value < 0){
+            e.target.classList.add('time-picker-error');
+            return false;
+        }
+
+        e.target.classList.remove('time-picker-error');
+        return true;
+    }
+
     useEffect(() => {
         if(timeType == 'AM'){
             document.querySelector('#time-picker-' + props.componentName + ' #time-am').classList.add('time-type--selected');
@@ -22,13 +42,13 @@ const TimePicker = (props) => {
         <div id={'time-picker-' + props.componentName} className='time-picker-container'>
             <div className='time-picker'>
                 <IoIosArrowUp />
-                <input type="number" id="hours-input" />
+                <input type="number" id="hours-input" onChange={(e) => hoursValidation(e)}/>
                 <IoIosArrowDown />
             </div>
             <span>:</span>
             <div className='time-picker'>
                 <IoIosArrowUp />
-                <input type="number" id="minutes-input" />
+                <input type="number" id="minutes-input" onChange={(e) => minutesValidation(e)} />
                 <IoIosArrowDown />
             </div>
             <div className='time-type'>
